43b85700949ec449ca59de301e890d2481502f93
vfaddey <vfaddey@yandex.ru>
2024-05-07 18:13:37 +0300

    Login + Regsister request

---
+++ src/common/requests/AddUserRequest.java
@@ -0,0 +1,8 @@
+package common.requests;
+
+public class AddUserRequest extends Request{
+
+    public AddUserRequest(String commandName) {
+        super(commandName);
+    }
+}
---
+++ src/common/requests/AuthenticateRequest.java
@@ -0,0 +1,7 @@
+package common.requests;
+
+public class AuthenticateRequest extends Request{
+    public AuthenticateRequest(String commandName) {
+        super(commandName);
+    }
+}
